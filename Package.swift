127ed4166f4be51876601c2b386490b4e6d39957
Shinya Kumagai <roomful.rooms@gmail.com>
2026-02-16 00:09:07 +0900

    deps: update Noora package version to 0.54.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.7.0"),
     .package(url: "https://github.com/swiftlang/swift-subprocess", exact: "0.2.1"),
-    .package(url: "https://github.com/tuist/Noora", exact: "0.53.0"),
+    .package(url: "https://github.com/tuist/Noora", exact: "0.54.1"),
   ],
   targets: [
     .executableTarget(
